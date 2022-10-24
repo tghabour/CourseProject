2f53d47bcc7908bc8cc13b6b04497bc38db7e977
Quan Nguyen <quannhoang@gmail.com>
2022-10-23 19:54:36 -0500

    Remove stuff that moved to quan brach

--- code/client/src/footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Footer() {
-    return (
-    <p>2022 &copy; by Team Early Birds</p>
-    )
-}
--- code/client/src/header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Header() {
-    return (
-    <h1>CS410 Project</h1>
-    )
-}
