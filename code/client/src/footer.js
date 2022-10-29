ca3a3e948a237fd6d7864c96f2e5f1828b510be6
Quan Nguyen <quannhoang@gmail.com>
2022-10-29 12:00:34 -0500

    Add download bar, text box

--- code/client/src/footer.js
+++ code/client/src/footer.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export function Footer() {
     return (
-    <p>2022 &copy; by Team Early Birds</p>
+        <div className='text-center h-10 mt-5 p-5 pb-10 shadow-inner'>
+              <p className='container mx-auto text-xs  text-gray-500'>2022 &copy; by Team Early Birds</p>  
+        </div>
+
     )
 }
