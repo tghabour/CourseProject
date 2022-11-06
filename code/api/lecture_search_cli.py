0bb9cad6d8cbc58270f8e11b03b4894da28bcaaa
Rick Suggs <suggsra@gmail.com>
2022-11-05 23:07:48 -0500

    move the corpora into the api directory and refactored search engine

---
+++ code/api/lecture_search_cli.py
@@ -0,0 +1,17 @@
+# import argparse
+import os
+import metapy
+from pprint import pprint
+from lecture_search import engine
+
+max_results = 5  # make extensible?
+query_txt = str(input("Enter CS410 search query: "))
+print()
+
+search_engine = engine.Engine()
+
+
+output = search_engine.query_corpus(query_txt, max_results)
+
+print("RESULT: ")
+pprint(output)
