ce3010fa2b8ff32161bc752822862b84b726f8b1
Rick Suggs <suggsra@gmail.com>
2022-11-12 09:12:24 -0600

    moved result into results component

--- code/client/src/results.js
+++ code/client/src/results.js
@@ -1,4 +1,16 @@
-import { Result } from "./result";
+const Result = ({ link, title, description }) => {
+  return (
+    <div className="button flex flex-col hover:bg-orange-50 rounded p-3 object-fit">
+      <a
+        href={link}
+        className="hover:underline text-sky-400 visited:text-indigo-800"
+      >
+        {title}
+      </a>
+      <span className="text-gray-500">{description}</span>
+    </div>
+  );
+};
 
 export const Results = ({ results }) => {
   return (
