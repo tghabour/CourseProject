fc76cd0fea84a13db29514b47d48a45b5b08d8f3
Rick Suggs <suggsra@gmail.com>
2022-11-12 08:30:30 -0600

    fixed invalid class props

--- code/client/src/body.js
+++ code/client/src/body.js
@@ -158,7 +158,7 @@ export function DownloadBar() {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-file-arrow-down"
+            className="bi bi-file-arrow-down"
             viewBox="0 0 16 16"
           >
             <path d="M8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z" />
@@ -174,7 +174,7 @@ export function DownloadBar() {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-file-arrow-down"
+            className="bi bi-file-arrow-down"
             viewBox="0 0 16 16"
           >
             <path d="M8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z" />
@@ -190,7 +190,7 @@ export function DownloadBar() {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-file-arrow-down"
+            className="bi bi-file-arrow-down"
             viewBox="0 0 16 16"
           >
             <path d="M8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z" />
