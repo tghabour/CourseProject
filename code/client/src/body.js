529d152a37c94bf7f821571b56b8a8abb65a7a4a
Rick Suggs <suggsra@gmail.com>
2022-11-12 08:24:07 -0600

    run formatter

--- code/client/src/body.js
+++ code/client/src/body.js
@@ -26,22 +26,23 @@ export class SearchBox extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      results: []
+      results: [],
     };
   }
   getAPI(query) {
-    const API_URL = "https://sea-turtle-app-7y54u.ondigitalocean.app/documents/"
+    const API_URL =
+      "https://sea-turtle-app-7y54u.ondigitalocean.app/documents/";
     //const API_URL = "http://localhost:8000/documents/"
     fetch(`${API_URL}?search=${query}`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (output) => {
-          console.log(query)
-          console.log(output)
+          console.log(query);
+          console.log(output);
           this.setState({
             isLoaded: true,
-            results: output.results
-          })
+            results: output.results,
+          });
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -49,24 +50,27 @@ export class SearchBox extends React.Component {
         (error) => {
           this.setState({
             isLoaded: true,
-            error
-          })
+            error,
+          });
         }
-      )
+      );
   }
   render() {
     const { error, isLoaded, results } = this.state;
-    console.log(results)
+    console.log(results);
     return (
       <div>
         <div className="container mx-auto flex flex-row mb-3">
           <input
-            id = "query"
+            id="query"
             className="container w-2/3 px-10 mr-2 focus:outline-2 outline-orange-300 bg-orange-100 rounded text-sm text-gray-500"
             type="text"
             placeholder="Search query..."
           />
-          <button onClick={()=> this.getAPI(document.getElementById('query').value)} className="container w-1/3 h-10 hover:bg-orange-600 bg-orange-300 text-orange-900 hover:text-white rounded text-sm">
+          <button
+            onClick={() => this.getAPI(document.getElementById("query").value)}
+            className="container w-1/3 h-10 hover:bg-orange-600 bg-orange-300 text-orange-900 hover:text-white rounded text-sm"
+          >
             Search Class Content
           </button>
         </div>
@@ -82,10 +86,8 @@ export class SearchBox extends React.Component {
           ))}
         </div>
       </div>
-
     );
   }
-
 }
 export function Result(props) {
   return (
@@ -100,7 +102,6 @@ export function Result(props) {
     </div>
   );
 }
-
 
 export function SearchResults() {
   return (
