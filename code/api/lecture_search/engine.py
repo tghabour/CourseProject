e8985add261e3be27c0c403e09b00da2d0a74f54
Tony Ghabour <45109940+tghabour@users.noreply.github.com>
2022-11-20 18:26:44 -0600

    added full text to output (#13)
    * added full text to output
    
    * pull in full text from file system
    
    Co-authored-by: Rick Suggs <suggsra@gmail.com>

--- code/api/lecture_search/engine.py
+++ code/api/lecture_search/engine.py
@@ -1,11 +1,14 @@
 # import argparse
 import os
 import metapy
+import requests
 from pprint import pprint
 
 
 class Engine:
-    CONFIG_PATH = "corpora/lectures/lectures-config.toml"
+    LECTURES_PATH = "corpora/lectures/"
+    CONFIG_PATH = LECTURES_PATH + "lectures-config.toml"
+    AWS_PATH = "https://cs410videostorage.s3.amazonaws.com/"
 
     def __init__(self):
         # make extensible?
@@ -46,7 +49,9 @@ class Engine:
             metadata = self.index.metadata(doc_id)
             video_id = metadata.get("video_id")
             file_identifier = metadata.get("AWS_file")
-            aws_path = "https://cs410videostorage.s3.amazonaws.com/"
+            txt_path = "{}{}.txt".format(self.LECTURES_PATH, video_id)
+            with open(txt_path) as file:
+                full_text = file.read()
 
             search_result = {
                 "03_video_id": video_id,
@@ -54,9 +59,10 @@ class Engine:
                 "02_score": float("{:.3f}".format(score)),
                 "00_rank": i + 1,
                 "04_title": metadata.get("title"),
-                "05_vid_path": "{0}{1}.mp4".format(aws_path, file_identifier),
-                "06_txt_path": "{0}{1}.txt".format(aws_path, file_identifier),
-                "07_pdf_path": "{0}{1}.pdf".format(aws_path, file_identifier),
+                "05_vid_path": "{0}{1}.mp4".format(self.AWS_PATH, file_identifier),
+                "06_txt_path": "{0}{1}.txt".format(self.AWS_PATH, file_identifier),
+                "07_pdf_path": "{0}{1}.pdf".format(self.AWS_PATH, file_identifier),
+                "08_full_txt": full_text,
             }
             search_results.append(search_result)
 
