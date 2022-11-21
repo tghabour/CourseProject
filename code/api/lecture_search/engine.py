a7dfc220e23429308fa87567c331d55e44e669f7
Rick Suggs <suggsra@gmail.com>
2022-11-20 18:25:12 -0600

    pull in full text from file system

--- code/api/lecture_search/engine.py
+++ code/api/lecture_search/engine.py
@@ -6,7 +6,9 @@ from pprint import pprint
 
 
 class Engine:
-    CONFIG_PATH = "corpora/lectures/lectures-config.toml"
+    LECTURES_PATH = "corpora/lectures/"
+    CONFIG_PATH = LECTURES_PATH + "lectures-config.toml"
+    AWS_PATH = "https://cs410videostorage.s3.amazonaws.com/"
 
     def __init__(self):
         # make extensible?
@@ -47,9 +49,9 @@ class Engine:
             metadata = self.index.metadata(doc_id)
             video_id = metadata.get("video_id")
             file_identifier = metadata.get("AWS_file")
-            aws_path = "https://cs410videostorage.s3.amazonaws.com/"
-            txt_path = "{0}{1}.txt".format(aws_path, file_identifier)
-            full_text = requests.get(txt_path).text
+            txt_path = "{}{}.txt".format(self.LECTURES_PATH, video_id)
+            with open(txt_path) as file:
+                full_text = file.read()
 
             search_result = {
                 "03_video_id": video_id,
@@ -57,9 +59,9 @@ class Engine:
                 "02_score": float("{:.3f}".format(score)),
                 "00_rank": i + 1,
                 "04_title": metadata.get("title"),
-                "05_vid_path": "{0}{1}.mp4".format(aws_path, file_identifier),
-                "06_txt_path": "{0}{1}.txt".format(aws_path, file_identifier),
-                "07_pdf_path": "{0}{1}.pdf".format(aws_path, file_identifier),
+                "05_vid_path": "{0}{1}.mp4".format(self.AWS_PATH, file_identifier),
+                "06_txt_path": "{0}{1}.txt".format(self.AWS_PATH, file_identifier),
+                "07_pdf_path": "{0}{1}.pdf".format(self.AWS_PATH, file_identifier),
                 "08_full_txt": full_text,
             }
             search_results.append(search_result)
