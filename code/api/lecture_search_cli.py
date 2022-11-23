64b9746fd5d932f1dfb6b47bb37da367826957b8
Maciej Wieczorek <maciej.wieczorek@gmail.com>
2022-11-23 17:13:28 +0100

    Refactor default value pattern
    h/t to Rick Suggs for suggesting a better design pattern for set default
    values on a parameter which avoids repeated calls to get() and fits on one
    line.

--- code/api/lecture_search_cli.py
+++ code/api/lecture_search_cli.py
@@ -3,16 +3,8 @@ from pprint import pprint
 from lecture_search import engine
 
 query_txt = str(input("Enter search query: "))
-
-corpus = str(input("Enter corpus [lectures]: "))
-if corpus == "":
-    corpus = "lectures"
-
-max_results = str(input("Enter number of search results [5]: "))
-if max_results == "":
-    max_results = 5
-else:
-    max_results = int(max_results)
+corpus = str(input("Enter corpus [lectures]: ")) or 'lectures'
+max_results = int(str(input("Enter number of search results [5]: ")) or 5)
 print()
 
 search_engine = engine.Engine(corpus)
