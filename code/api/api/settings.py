a5172bbe800ccd2fe5cb33d8134b7351169b6919
Rick Suggs <suggsra@gmail.com>
2022-11-25 23:00:59 -0600

    cleanup

--- code/api/api/settings.py
+++ code/api/api/settings.py
@@ -14,7 +14,9 @@ import os
 import logging
 from django.core.management.utils import get_random_secret_key
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
