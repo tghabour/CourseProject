3fd7af3cb0aedbbc7feb9f421aac3d3408c4c04d
Rick Suggs <111754987+rsuggs-uiuc-mcs@users.noreply.github.com>
2022-11-27 16:14:19 -0600

    cleanup SearchBox component (#22)
    * cleanup SearchBox component
    
    * extracted urls to .env files
    
    * added newlines
    
    Co-authored-by: Rick Suggs <suggsra@gmail.com>

--- code/client/src/text-box.js
+++ code/client/src/text-box.js
@@ -1,9 +1,7 @@
 export const TextBox = (props) => {
   return (
     <div className="container mx-auto mt-3 p-3 max-h-80 overflow-x-auto rounded-md border-2 border-gray-300 text-sm text-gray-500 ">
-      <p className="m-2">
-        {props.text}
-      </p>
+      <p className="m-2">{props.text}</p>
     </div>
   );
 };
