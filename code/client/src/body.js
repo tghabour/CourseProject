2056f7c9c251ba1553b3d02718c29472bde0cfe3
Quan Nguyen <42251023+quannhoang@users.noreply.github.com>
2022-11-27 12:22:54 -0600

    Add search options and comments (#21)
    * Add loading results when clicking links
    
    * Add local text files
    
    * Add next and previous
    
    * Add start time and fix text loading issue by using full-text returned by the api
    
    * Add search options
    
    * Remove unused code
    
    * Add high level comments and clean up

--- code/client/src/body.js
+++ code/client/src/body.js
@@ -1,21 +1,11 @@
-import { DownloadBar } from "./download-bar";
-import { TextBox } from "./text-box";
-import { VideoPlayer } from "./video-player";
 import { SearchBox } from "./search-box";
+import { SearchOptions } from "./search-options";
 
 export const Body = () => {
   return (
     <div className="">
+      <SearchOptions />
       <SearchBox />
-      <div className="flex flex-row w-full">
-        <div className="container w-1/3"></div>
-        <div className="container mx-auto w-full">
-          <VideoPlayer />
-          <DownloadBar />
-        </div>
-        <div className="container w-1/3"></div>
-      </div>
-      <TextBox />
     </div>
   );
 };
--- code/client/src/video-player.js
+++
@@ -1,9 +0,0 @@
-import { ResponsivePlayer } from "./player";
-
-export const VideoPlayer = () => {
-  return (
-    <div className=" mb-3">
-      <ResponsivePlayer />
-    </div>
-  );
-};
