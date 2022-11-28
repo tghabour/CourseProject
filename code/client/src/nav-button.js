2ee16d327adefa74871ea72ac4fbde3234b56feb
Rick Suggs <111754987+rsuggs-uiuc-mcs@users.noreply.github.com>
2022-11-27 19:10:44 -0600

    more FE cleanup and refactoring (#23)
    Co-authored-by: Rick Suggs <suggsra@gmail.com>

---
+++ code/client/src/nav-button.js
@@ -0,0 +1,10 @@
+export function NavButton({ label, handleClick }) {
+  return (
+    <button
+    onClick={handleClick}
+    className="pl-5 inline-flex w-20 hover:bg-orange-600 bg-orange-300 text-orange-900 hover:text-white text-xs p-1 rounded mr-4"
+  >
+    {label}
+  </button>
+  );
+}
